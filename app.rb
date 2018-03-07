d9ae53b741caa3228ccd297ba0f4dfda71d29ec0
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-07 00:19:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,16 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
+	
+	get '/hello' do
+		erb :index
+	end
+	
+	get '/goodbye' do
+		erb :index
+	end
+	
+	get '/date' do
+		erb :index
+	end
 end
